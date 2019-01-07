02f9bed504b4d7f933b69fada80a6d492a2ff6c3
arora-72 <indresh.arora2016@vitstudent.ac.in>
2019-01-07 15:16:38 +0530

    Initial Commit

---
+++ riviera-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  riviera-19
+//
+//  Created by Indresh Arora on 07/01/19.
+//  Copyright © 2019 Indresh Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
